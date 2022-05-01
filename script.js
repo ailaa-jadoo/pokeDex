441048782273ad7b49d99c86d1cf712870edc4f1
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2022-05-01 16:15:35 +0530

    added dark color to the colors array

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const colors = {
     psychic: '#eaeda1',
     flying: '#F5F5F5',
     fighting: '#E6E0D4',
-    normal: '#F5F5F5'
+    normal: '#F5F5F5',
+    dark: '#73736c'
 };
 const main_types = Object.keys(colors);
 
